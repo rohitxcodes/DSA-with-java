448b08df42f334875e5b47a57fbc07e01c7d9fae
Rohit Kumar <146556108+rohitxcodes@users.noreply.github.com>
2026-07-05 22:21:50 +0530

    Time: 2151 ms (5.04%), Space: 49 MB (22.16%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,54 +1,23 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int count = 0;
-        int currentSum = 0;
+        int count=0;
+        int[] prefix=new int[nums.length];
+        prefix[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            for(int j=i;j<nums.length;j++){
+                int sum;
+                if(i==0) {sum=prefix[j];}
+                else{sum=prefix[j]-prefix[i-1];}
 
-        int size = 1 << 15;
-        int mask = size - 1;
-
-        int[] keys = new int[size];
-        int[] values = new int[size];
-        boolean[] used = new boolean[size];
-
-        put(keys, values, used, mask, 0, 1);
-
-        for (int num : nums) {
-            currentSum += num;
-            int target = currentSum - k;
-
-            int h = hash(target) & mask;
-            while (used[h]) {
-                if (keys[h] == target) {
-                    count += values[h];
-                    break;
+                if(sum==k){
+                    count++;
                 }
-                h = (h + 1) & mask;
             }
-
-            put(keys, values, used, mask, currentSum, 1);
         }
 
         return count;
-    }
-
-    private void put(int[] keys, int[] values,
-                     boolean[] used, int mask,
-                     int key, int val) {
-        int h = hash(key) & mask;
-
-        while (used[h] && keys[h] != key) {
-            h = (h + 1) & mask;
-        }
-
-        keys[h] = key;
-        values[h] += val;
-        used[h] = true;
-    }
-
-    private int hash(int x) {
-        x = ((x >>> 16) ^ x) * 0x45d9f3b;
-        x = ((x >>> 16) ^ x) * 0x45d9f3b;
-        x = ((x >>> 16) ^ x);
-        return x;
     }
 }
