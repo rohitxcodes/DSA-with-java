ba2ac7e0d52e537cf0f910c9a6eacca0705621ea
Rohit Kumar <146556108+rohitxcodes@users.noreply.github.com>
2026-08-21 10:33:22 +0530

    Time: 0 ms (100%), Space: 44.1 MB (88.53%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,71 +10,29 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) {
-            return list2;
-        }
 
-        ListNode temp=list1;
-        while(temp.next!=null){
-            temp=temp.next;
-        }
-        temp.next=list2;
-        
-        return sort(list1);
-    }
-    public ListNode sort(ListNode list){
-        if(list==null||list.next==null){
-            return list;
-        }
-        
-        ListNode mid=findMid(list);
-        ListNode left=list;
-        ListNode right=mid.next;
-        mid.next=null;
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
 
-        left=sort(left);
-        right=sort(right);
+        while (list1 != null && list2 != null) {
 
-        ListNode result=merge(left,right);
-        return result;
-    }
-    public ListNode findMid(ListNode list){
-        ListNode slow=list;
-        ListNode fast=list.next;
-        while(fast!=null&& fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        return slow;
-    }
-    public ListNode merge(ListNode left,ListNode right){
-        if(left==null) return right;
-        if(right==null) return left;
-
-        ListNode ans=new ListNode(-1);
-        ListNode temp=ans;
-        
-        while(left!=null&& right!=null){
-            if(left.val<right.val){
-                temp.next=left;
-                temp=left;
-                left=left.next;
-            }else{
-                temp.next=right;
-                temp=right;
-                right=right.next;
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
             }
+
+            tail = tail.next;
         }
-        while (left!=null){
-            temp.next=left;
-            temp=left;
-            left=left.next;
+
+        if (list1 != null) {
+            tail.next = list1;
+        } else {
+            tail.next = list2;
         }
-        while (right!=null){
-            temp.next=right;
-            temp=right;
-            right=right.next;
-        }
-        return ans.next;
+
+        return dummy.next;
     }
 }
