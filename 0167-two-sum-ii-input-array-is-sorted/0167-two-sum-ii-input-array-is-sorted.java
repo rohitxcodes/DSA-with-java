eb1c172b7b5d2f3dd74271ca82f8cf9ac33f01de
Rohit Kumar <146556108+rohitxcodes@users.noreply.github.com>
2026-08-30 07:58:30 +0530

    Time: 2 ms (95.64%), Space: 48.6 MB (10.68%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,34 +1,22 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int[] ans = new int[2];
-        
-        for(int i=0;i<numbers.length;i++){
-            int comp=target-numbers[i];
-            int index = bs(numbers,comp,i+1,numbers.length-1);
+        int s = 0;
+        int e = numbers.length - 1;
 
-            if(index > -1){
-                ans[0]=i+1;
-                ans[1]=index+1;
+        while (s < e) {
+            int sum = numbers[s] + numbers[e];
+
+            if (sum == target) {
+                return new int[]{s + 1, e + 1};
+            }
+
+            if (sum < target) {
+                s++;
+            } else {
+                e--;
             }
         }
 
-        return ans;
-    }
-
-    public int bs(int[] arr, int target, int s, int e) {
-        if (s > e)
-            return -1;
-
-        int mid = s + (e - s) / 2;
-
-        if (arr[mid] == target) {
-            return mid;
-        }
-
-        if (arr[mid] < target) {
-            return bs(arr, target, mid + 1, e);
-        } else {
-            return bs(arr, target, s, mid - 1);
-        }
+        return new int[]{-1, -1};
     }
 }
