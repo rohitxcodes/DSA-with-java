f25fd4488d336a0a4f87a2cd732d4f6a553d05c9
Rohit Kumar <146556108+rohitxcodes@users.noreply.github.com>
2026-08-31 17:52:30 +0530

    Time: 4 ms (99.56%), Space: 47 MB (83.03%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+
+                if (grid[i][j] == 1) {
+
+                    // up
+                    if (i == 0 || grid[i - 1][j] == 0)
+                        perimeter++;
+
+                    // down
+                    if (i == grid.length - 1 || grid[i + 1][j] == 0)
+                        perimeter++;
+
+                    // left
+                    if (j == 0 || grid[i][j - 1] == 0)
+                        perimeter++;
+
+                    // right
+                    if (j == grid[0].length - 1 || grid[i][j + 1] == 0)
+                        perimeter++;
+                }
+            }
+        }
+
+        return perimeter;
+    }
+}
